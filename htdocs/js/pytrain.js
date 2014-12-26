3bd2fcdcfe99628801f94050a0a6f7d233c6b19f
kinders <kinders@glastop>
2014-12-26 17:23:40 +0100

    Basic web server in python. Allow basic oonclick switching.

---
+++ htdocs/js/pytrain.js
@@ -0,0 +1,56 @@
+var pytrain = function() {
+
+	
+    var svg = document.getElementById("railtrack_baselayout");
+	
+    var svgDoc = (svg.contentWindow || svg.contentDocument);
+    if (svgDoc.document) svgDoc = svgDoc.document;
+    
+    var switches = [];
+    
+    var define_switch = function(lbl) {
+	   
+    	var rwswitch = {
+    			
+    			link:  svgDoc.getElementById(lbl+"_link"),
+    			node_0:  svgDoc.getElementById(lbl+"_0"),
+    			node_1:  svgDoc.getElementById(lbl+"_1"),
+
+    			state: 0,
+    			
+    			showState: function() {
+    				this.node_0.style.display= this.state==0 ? "inline" : "none";
+    				this.node_1.style.display= this.state!=0 ? "inline" : "none";
+    			},
+    			
+    			toggle: function() {
+    				this.state = this.state!=0 ? 0 : 1;
+    				this.showState();
+    			}
+    	};
+    	
+    	rwswitch.link.onclick= function() {
+    		rwswitch.toggle();
+    	};
+    	
+    	rwswitch.showState();
+    	return rwswitch;
+    } 
+    
+    for (var i=0;i<2;++i) {
+    	
+    	var lbl = "layer_switch_" + (i+1);
+    	   	
+        switches.push(define_switch(lbl));
+    }
+    
+    
+    
+    
+//	var layer_switch_1_0 = svgDoc.getElementById("layer_switch_1_0");
+//	var layer_switch_2_1 = svgDoc.getElementById("layer_switch_2_1");
+
+//	layer_switch_1_0.style.display="none";
+//	layer_switch_2_1.style.display="none";
+
+};
--- src/pytrain/Main.py
+++ src/pytrain/Main.py
@@ -1,31 +1,11 @@
-from xmlrpc.server import SimpleXMLRPCServer
-from xmlrpc.server import SimpleXMLRPCRequestHandler
+import http.server
+import socketserver
 
-# Restrict to a particular path.
-class RequestHandler(SimpleXMLRPCRequestHandler):
-    rpc_paths = ('/RPC2',)
+PORT = 8000
 
-# Create server
-server = SimpleXMLRPCServer(("localhost", 8000),
-                            requestHandler=RequestHandler)
-server.register_introspection_functions()
+Handler = http.server.SimpleHTTPRequestHandler
 
-# Register pow() function; this will use the value of
-# pow.__name__ as the name, which is just 'pow'.
-server.register_function(pow)
+httpd = socketserver.TCPServer(("", PORT), Handler)
 
-# Register a function under a different name
-def adder_function(x,y):
-    return x + y
-server.register_function(adder_function, 'add')
-
-# Register an instance; all the methods of the instance are
-# published as XML-RPC methods (in this case, just 'div').
-class MyFuncs:
-    def div(self, x, y):
-        return x // y
-
-server.register_instance(MyFuncs())
-
-# Run the server's main loop
-server.serve_forever()
+print("serving at port", PORT)
+httpd.serve_forever()
