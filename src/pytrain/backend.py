5866d39a4e88a89eb89e58606125fc89ebf07b1e
Wolfgang Glas <pi@raspberrypi.(none)>
2015-01-06 12:31:46 +0100

    Fixed port assignments, initialize output pins

--- src/pytrain/backend.py
+++ src/pytrain/backend.py
@@ -21,8 +21,10 @@ class Backend(object):
         # GPIO PIN numbers of the switches
         # first is the 8-port breakout board, second the 4-port breakout board
         GPIO.setmode(GPIO.BOARD)
-        self.gpioMap = [ 3,5,7,11,13,15,19,21,  14,15,18,23 ]
+        self.gpioMap = [ 3,5,7,11,13,15,19,21,  10,12,16,18 ]
         for pinid in self.gpioMap:
+            print("Initializing PIN ",pinid)
+            GPIO.setup(pinid,GPIO.OUT)
             GPIO.output(pinid,0);
         
     def toggleSwitch(self,i):
